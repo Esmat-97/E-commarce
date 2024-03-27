f02f29657bd298b51eba0a5c4a2ba24bc67d4382
mohamedesmat1997 <158211063+63456356@users.noreply.github.com>
2024-03-27 17:49:39 +0200

    insert data from register

--- myIonicApp/src/app/register/register.page.ts
+++ myIonicApp/src/app/register/register.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
+  standalone:true,
+  imports:[FormsModule ,CommonModule ,IonicModule],
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage  {
 
-  constructor() { }
+  constructor(private http:HttpClient){}
 
-  ngOnInit() {
+  signData:any={
+    username:'',
+    email:'',
+    password:''
+  };
+
+  submitForm(main: any) {
+  
+    this.signData=main.value;
+  
+    console.log('Form data submitted:', this.signData);
+
+    this.http.post('http://localhost:1999/api',this.signData).subscribe( response =>{
+
+    console.log('Data sent successfully');
+   
+    })
   }
+
 
 }
